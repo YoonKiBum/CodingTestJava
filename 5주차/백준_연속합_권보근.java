87abd6a67710cc2a9439063f2d858d1d8d2e7495
kwonbokeun <43335683+kwonbokeun@users.noreply.github.com>
2023-02-09 00:29:39 +0900

    Create 백준_연속합_권보근.java

---
+++ 5주차/백준_연속합_권보근.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int last_val = 0, max = -1001;
+        for(int i=0; i<N; i++){
+            int cur = Integer.parseInt(st.nextToken());
+
+            last_val = Math.max(last_val + cur, cur);
+            max = Math.max(max, last_val);
+        }
+        System.out.println(max);
+    }
+}
